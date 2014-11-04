4ba84529cd9f947f9a1f792c2e9426072117935c
Sam Richard <snugug@gmail.com>
2014-11-04 10:30:46 -0500

    Sass Gulp tasks

---
+++ tasks/sass.js
@@ -0,0 +1,61 @@
+'use strict';
+
+//////////////////////////////
+// Requires
+//////////////////////////////
+var gutil = require('gulp-util'),
+    run = require('gulp-run'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+//////////////////////////////
+// Internal Vars
+//////////////////////////////
+var toSass = [
+  'patterns/**/*.scss',
+  'patterns/**/*.sass'
+];
+
+//////////////////////////////
+// Export
+//////////////////////////////
+module.exports = function (gulp, sassPaths) {
+  // Set value of paths to either the default or user entered
+  sassPaths = sassPaths || toSass;
+
+  //////////////////////////////
+  // Encapsulate task in function to choose path to work on
+  //////////////////////////////
+  var sassTask = function (path) {
+    return gulp.src(path)
+      .pipe(run('bundle exec compass compile'))
+      .pipe(reload({stream: true}));
+  }
+
+  //////////////////////////////
+  // Core Task
+  //////////////////////////////
+  gulp.task('sass', function () {
+    return sassTask(sassPaths);
+  });
+
+  //////////////////////////////
+  // Watch Task
+  //////////////////////////////
+  gulp.task('sass-watch', function () {
+    return gulp.watch(sassPaths)
+      .on('change', function (event) {
+	// Add absolute and relative (to Gulpfile) paths
+	event.path = {
+	  absolute: event.path,
+	  relative: event.path.replace(__dirname.replace('/tasks', '') + '/', '')
+	}
+
+	// Notify user of the change
+	gutil.log('File ' + gutil.colors.magenta(event.path.relative) + ' was ' + event.type);
+
+	// Call the task
+	return sassTask(event.path.absolute);
+      });
+  });
+}
