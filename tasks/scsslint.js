9e3b60b16e6973ba503d8c917b24edfca8178f44
Sam Richard <snugug@gmail.com>
2014-11-10 18:49:24 -0500

    Custom reporter, updated name
    Using ESLint's stylish reporter, changed the name from `sass-lint` to `scsslint`

--- tasks/scsslint.js
+++ tasks/scsslint.js
@@ -4,7 +4,9 @@
 // Requires
 //////////////////////////////
 var gutil = require('gulp-util'),
-    lint = require('gulp-scss-lint');
+    ifElse = require('gulp-if-else'),
+    stylish = require('eslint/lib/formatters/stylish'),
+    scsslint = require('gulp-scss-lint');
 
 //////////////////////////////
 // Internal Vars
@@ -22,26 +24,63 @@ module.exports = function (gulp, sassLintPaths) {
   sassLintPaths = sassLintPaths || toSassLint;
 
   //////////////////////////////
+  // Custom Reporter
+  //////////////////////////////
+  var sassLintReporter = function (file) {
+    if (!file.scsslint.success) {
+      var report = {};
+
+      report.filePath = file.path;
+      report.messages = [];
+
+      // Add Filename
+      file.scsslint.issues.forEach(function (v) {
+        var message = {};
+
+        // Set severity
+        if (v.severity === 'error') {
+          message.severity = 2;
+        }
+        else {
+          message.severity = 1;
+        }
+
+        // Set line and columns
+        message.line = v.line;
+        message.column = v.column;
+        message.message = v.reason;
+        message.ruleId = v.linter;
+
+        report.messages.push(message);
+      });
+      // Pass to Stylish
+      gutil.log(stylish([report]));
+    }
+  };
+
+  //////////////////////////////
   // Encapsulate task in function to choose path to work on
   //////////////////////////////
-  var sassLintTask = function (path) {
+  var sassLintTask = function (path, fail) {
     return gulp.src(path)
-      .pipe(lint({
-        'bundleExec': true
-      }));
+      .pipe(scsslint({
+        'bundleExec': true,
+        'customReport': sassLintReporter
+      }))
+      .pipe(ifElse(fail === true, scsslint.failReporter));
   }
 
   //////////////////////////////
   // Core Task
   //////////////////////////////
-  gulp.task('sass-lint', function () {
-    return sassLintTask(sassLintPaths);
+  gulp.task('scsslint', function () {
+    return sassLintTask(sassLintPaths, true);
   });
 
   //////////////////////////////
   // Watch Task
   //////////////////////////////
-  gulp.task('sass-lint-watch', function () {
+  gulp.task('scsslint-watch', function () {
     return gulp.watch(sassLintPaths)
       .on('change', function (event) {
         // Add absolute and relative (to Gulpfile) paths
